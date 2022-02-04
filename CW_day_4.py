ed8ead1aeaff057a1c523043c0ed6fedec6df9f5
MValencia111 <70298511+MValencia111@users.noreply.github.com>
2022-02-04 09:17:54 -0600

    cleaner version of day 4 challenge

--- CW_day_4.py
+++ CW_day_4.py
@@ -19,29 +19,7 @@ def switch_it_up(number: int) -> str:
 
     #your code here
 
-    dict_num = {
-
-        0: 'Zero',
-
-        1: 'One',
-
-        2: 'Two',
-
-        3: 'Three',
-
-        4: 'Four',
-
-        5: 'Five',
-
-        6: 'Six',
-
-        7: 'Seven',
-
-        8: 'Eight',
-
-        9: 'Nine'
-
-        }
+    dict_num = {0: 'Zero', 1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', 6: 'Six', 7: 'Seven', 8: 'Eight',9: 'Nine'}
 
     return dict_num.get(number)
 
